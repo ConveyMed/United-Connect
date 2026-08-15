23d11993dc313525838933d4ac6079e0e8c9169b
Chase Kellis <chase@cardchase.org>
2026-08-15 08:25:41 -0700

    Android: display name U Connect under icon to match iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.unitedconnect.app',
-  appName: 'United Connect',
+  appName: 'U Connect',
   webDir: 'build',
   ios: {
     contentInset: 'never',
